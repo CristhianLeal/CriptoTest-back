d096355f864a63b8dcfb571227b8ac118acd7c18
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-08-20 17:28:13 -0300

    Avoid storing duplicate information

--- src/controllers/cripto.controllers.js
+++ src/controllers/cripto.controllers.js
@@ -3,19 +3,52 @@ import { CovalentApi } from '../api/covalentApi.js'
 
 export const getCripto = async (req, res) => {
   try {
-    const response = await CovalentApi.get('/pricing/historical_by_addresses_v2/eth-mainnet/USD/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/?from=2023-08-08&to=2023-08-18',{headers:{'Authorization': `${process.env.API_KEY}`}})
-    const cryptoData = response.data
-    const newCryptoData = new CryptoData({
-      contractName: cryptoData.data[0].contract_name,
-      logoUrl: cryptoData.data[0].logo_url,
-      quoteCurrency: cryptoData.data[0].quote_currency,
-      prices: cryptoData.data[0].prices.map(price => ({
-        price: price.price,
-        date: price.date
-      }))
-    })
-    await newCryptoData.save()
-    res.json(newCryptoData)
+    const existingCryptoData = await CryptoData.findOne({ contractName:'Ether'})
+    if (existingCryptoData) {
+      const today = new Date();
+      const existingDate = existingCryptoData.createdAt
+      const todayYear = today.getFullYear();
+      const todayMonth = today.getMonth();
+      const todayDay = today.getDate();
+      const existingYear = existingDate.getFullYear();
+      const existingMonth = existingDate.getMonth();
+      const existingDay = existingDate.getDate();
+      if (
+        todayYear === existingYear &&
+        todayMonth === existingMonth &&
+        todayDay === existingDay
+      ) {
+        res.json(existingCryptoData)
+      }else {
+        const response = await CovalentApi.get('/pricing/historical_by_addresses_v2/eth-mainnet/USD/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/?from=2023-08-08&to=2023-08-18',{headers:{'Authorization': `${process.env.API_KEY}`}})
+        const cryptoData = response.data
+        const newCryptoData = new CryptoData({
+          contractName: cryptoData.data[0].contract_name,
+          logoUrl: cryptoData.data[0].logo_url,
+          quoteCurrency: cryptoData.data[0].quote_currency,
+          prices: cryptoData.data[0].prices.map(price => ({
+            price: price.price,
+            date: price.date
+          }))
+        })
+        await newCryptoData.save()
+        res.json(newCryptoData)
+      }
+    } else {
+      const response = await CovalentApi.get('/pricing/historical_by_addresses_v2/eth-mainnet/USD/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/?from=2023-08-08&to=2023-08-18',{headers:{'Authorization': `${process.env.API_KEY}`}})
+      const cryptoData = response.data
+      const newCryptoData = new CryptoData({
+        contractName: cryptoData.data[0].contract_name,
+        logoUrl: cryptoData.data[0].logo_url,
+        quoteCurrency: cryptoData.data[0].quote_currency,
+        prices: cryptoData.data[0].prices.map(price => ({
+          price: price.price,
+          date: price.date
+        }))
+      })
+      await newCryptoData.save()
+      res.json(newCryptoData)
+    }
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'problema al guardar los datos',})
