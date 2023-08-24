6693e26e9263b83f8bb1e025134418381f6f331c
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-08-23 22:39:17 -0300

    update eslint config

--- src/api/covalentApi.js
+++ src/api/covalentApi.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const CovalentApi = axios.create({
-  baseURL:'https://api.covalenthq.com/v1',
+  baseURL: 'https://api.covalenthq.com/v1'
 })
